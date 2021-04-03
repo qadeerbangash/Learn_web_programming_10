d418e78fd5916ff79c7cb42e067514a10574301b
Qadeer Hussain <qadeeerhussain494@gmail.com>
2021-04-03 11:02:36 +0500

    Just Practice

---
+++ app.js
@@ -0,0 +1,88 @@
+//  var a=+prompt("enter your age","your age is:")
+// alert(a);
+// var b=a+10;
+// console.log("The Output Is",+b);
+
+// var x="Enter Value";
+// var y="Your Value is :";
+// var a=+prompt(x,y);
+// var b= a+10;
+// console.log("The output is ",+" "+ b);
+
+
+// var x="Enter Value";
+// var y="Your Value is :";
+// var a=+prompt(x,y);
+// var b= a+40;
+// document.write("the ans: is "+" "+b);
+
+
+// var age =+prompt("Enter your age")
+// if(age==23){
+//     alert("your age is equal");
+// }
+// else if(age <18){
+//     alert("your age is less" )
+// }
+// else{
+//     alert("you are no more")
+// }
+
+
+ 
+// var age=+prompt("Enter Your Age")
+// var gender=prompt("Enter Gender")
+// if(age>18 && gender=="male")
+// {
+//  alert("You are allowed")
+
+// }
+// else{
+//     alert("You are not Allowed")
+// }
+
+
+
+// var a="5";
+// if(a==5){
+//     alert("true")
+// }
+// else{
+//     alert("false")
+// }
+
+// var x=5;
+// var y=10;
+// var a=30;
+// var b=15;
+// if(x+y == a-b){
+//     alert("correct")
+// }
+// else{
+//     alert("Incorrect")
+// }
+
+
+//   var student =["Qadeer","Ali","Ghous"]
+//   alert("Welcome"+" "+ student[0])
+
+
+// var e=[];
+// e[0]="Qadeer";
+// e[1]="Ali"
+// alert(e)
+// document.write(e)
+// console.log(e) 
+
+
+
+  var student =["Qadeer","Ali","Ghous","M","N","O"]
+//   student.pop()
+// student.push("Hadi")
+// student.shift()
+// student.unshift("Hadi","Muzahir")
+// student.splice(0,2,"Hadi","Abbas","Hussain")
+// student.splice(1,1)
+ var cs=student.slice(1,5)
+  console.log(student)
+  console.log("student is "+cs)
